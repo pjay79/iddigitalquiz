7b4a498ba53dbcd1e37b37f6a3af031ceff9141b
pjay79 <praveen.jayarajan@gmail.com>
2016-12-12 11:02:03 +1100

    Setup ID Digital Quiz Project

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+  // Force scroll to top on refresh + force cache reload
+  $(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
+  $(window).location.reload(true);
+  });
+  // Set 100% viewport width/height to div's
+  var fullscreen = function(){
+    $('main').css({width: $(window).width(), height: $(window).height()});
+  };
+  fullscreen();
+  // Run the function again in case of window resize
+  $(window).on('resize', function() {
+    fullscreen();
+  });
+  // Hover touch
+  $('body').bind('touchstart', function(){});
