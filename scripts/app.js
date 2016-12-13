a34c67bcc0d3fe7d2293769153450d4cc0e12cf3
pjay79 <praveen.jayarajan@gmail.com>
2016-12-14 07:31:04 +1100

    Add hamburger menu icon and overlay for smaller screens

--- scripts/app.js
+++ scripts/app.js
@@ -14,5 +14,23 @@
   });
   // Hover touch
   $('body').bind('touchstart', function(){});
-
+  // Toggle drop down menu
   $('.dropdown-toggle').dropdown();
+  // Hamburger menu overlay
+  var overlay = new TimelineMax({paused:true});
+      overlay.to($('.header__hamburger-menu--overlay'), 0.25, { autoAlpha: 1 });
+             //.staggerFrom($('nav ul li a'), 0.25, { autoAlpha:0, scale: 1.5, cycle: {x: [-25, 25]}, ease: Expo.easeOut}, 0.25);
+  $('.header__hamburger-menu').on('click', function() {
+    if (!$(this).hasClass('active')) {
+      $(this).addClass('active');
+      overlay.timeScale(1).play();
+    } else {
+      $(this).removeClass('active');
+      overlay.timeScale(5).reverse();
+    }
+  });
+  // Hamburger menu overlay - close on clicking link
+  $('nav li a').on('click', function() {
+    $('.header__hamburger-menu').removeClass('active');
+    overlay.timeScale(5).reverse();
+  });
