b4099bcb853c57f3372d1911e69305601d48dc0b
pjay79 <praveen.jayarajan@gmail.com>
2016-12-15 20:52:43 +1100

    Change header paddings

--- scripts/app.js
+++ scripts/app.js
@@ -72,13 +72,13 @@
       $("html, body").animate({scrollTop: 0}, 1000);
   });
 
-  var movementStrength = 25;
-  var height = movementStrength / $(window).height();
-  var width = movementStrength / $(window).width();
-  $("#top-image").mousemove(function(e){
-            var pageX = e.pageX - ($(window).width() / 2);
-            var pageY = e.pageY - ($(window).height() / 2);
-            var newvalueX = width * pageX * -1 - 25;
-            var newvalueY = height * pageY * -1 - 50;
-            $('#top-image').css("background-position", newvalueX+"px" + newvalueY+"px");
+  $('main').mousemove(function( event ) {
+
+  var containerWidth = $(this).innerWidth(),
+      containerHeight = $(this).innerHeight(),
+      mousePositionX = (event.pageX / containerWidth) * 20,
+      mousePositionY = (event.pageY /containerHeight) * 20;
+
+  $(this).css('background-position', mousePositionX + '%' + ' ' + mousePositionY + '%');
+
   });
